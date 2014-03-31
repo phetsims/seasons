f9f5953914b07185fd496599b27a9a60b03ae180
Sam Reid <reids@colorado.edu>
2014-03-31 15:02:15 -0600

    added accordion boxes for the top left

--- js/intensity/view/IntensityView.js
+++ js/intensity/view/IntensityView.js
@@ -10,9 +10,18 @@ define( function( require ) {
 
   var inherit = require( 'PHET_CORE/inherit' );
   var ScreenView = require( 'JOIST/ScreenView' );
+  var AccordionBox = require( 'SUN/AccordionBox' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
 
   function IntensityView( model ) {
     ScreenView.call( this, { renderer: 'svg' } );
+
+    var intensityBox = new AccordionBox( new Text( 'hello' ), {title: 'Intensity', initiallyOpen: false, fill: 'black', titleFill: 'white', stroke: 'white'} );
+
+    var secondBox = new AccordionBox( new Text( 'hello again' ), {title: '-', initiallyOpen: false, fill: 'black', titleFill: 'white', stroke: 'white'} );
+
+    this.addChild( new HBox( {x: 10, y: 10, children: [intensityBox, secondBox], spacing: 20} ) );
   }
 
   return inherit( ScreenView, IntensityView );

d14bbb66b8d2836e9941cb5d09f8a50aae76cd70
Sam Reid <reids@colorado.edu>
2014-03-31 15:07:04 -0600

    added reset all button

--- js/intensity/view/IntensityView.js
+++ js/intensity/view/IntensityView.js
@@ -13,6 +13,7 @@ define( function( require ) {
   var AccordionBox = require( 'SUN/AccordionBox' );
   var Text = require( 'SCENERY/nodes/Text' );
   var HBox = require( 'SCENERY/nodes/HBox' );
+  var ResetAllButton2 = require( 'SUN/experimental/buttons/ResetAllButton2' );
 
   function IntensityView( model ) {
     ScreenView.call( this, { renderer: 'svg' } );
@@ -22,6 +23,9 @@ define( function( require ) {
     var secondBox = new AccordionBox( new Text( 'hello again' ), {title: '-', initiallyOpen: false, fill: 'black', titleFill: 'white', stroke: 'white'} );
 
     this.addChild( new HBox( {x: 10, y: 10, children: [intensityBox, secondBox], spacing: 20} ) );
+
+    var resetAllButton = new ResetAllButton2( {right: this.layoutBounds.right - 10, bottom: this.layoutBounds.bottom - 10} );
+    this.addChild( resetAllButton );
   }
 
   return inherit( ScreenView, IntensityView );

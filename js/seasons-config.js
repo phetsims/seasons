c70edb7cccc9bd02861b533cb25dc4abdc22d5fe
Sam Reid <reids@colorado.edu>
2015-02-03 20:52:46 -0700

    Added cache buster as an option & standardized formatting for config files, see https://github.com/phetsims/joist/issues/196

--- js/seasons-config.js
+++ js/seasons-config.js
@@ -35,5 +35,6 @@ require.config( {
     SEASONS: '.'
   },
 
-  urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.phetcommon.getCacheBusterArgs()
 } );

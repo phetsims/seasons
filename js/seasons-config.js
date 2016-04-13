67ba68c8380cd4a933cbbfcb6aba967105fcfdd3
samreid <reids@colorado.edu>
2016-04-13 13:30:56 -0600

    Moved TandemDragHandler to tandem/ and added tandem to config, see https://github.com/phetsims/tandem/issues/9

--- js/seasons-config.js
+++ js/seasons-config.js
@@ -32,7 +32,8 @@ require.config( {
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
-
+    TANDEM: '../../tandem/js',
+    
     // sim code
     SEASONS: '.'
   },
